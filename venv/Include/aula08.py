5b52acc9c8b2d5daff093a00a82b804a4f1f833e
Mezengas <alexfonsecaramos@hotmail.com>
2022-10-28 12:18:56 -0300

    untitled

---
+++ venv/Include/aula08.py
@@ -0,0 +1,9 @@
+import math
+num = float(input(" Entre com um número : "))
+dobro = 2*num
+triplo = 3*num
+raizq = math.srqt(num)
+
+print(" O dobro de {} é {}.".format(num,dobro))
+print(" O triplo de {} é {}.".format(num,triplo))
+print(" Raiz quadrada de {} é {}.".format(num,raizq))
